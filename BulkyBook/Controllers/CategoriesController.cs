71a758d2da0ac479bc3d3c28f0ba40846b11e0e9
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-02-04 20:36:26 -0500

    Updated Category Index

--- BulkyBook/Controllers/CategoriesController.cs
+++ BulkyBook/Controllers/CategoriesController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _db.Categories;
+
+            return View(categories);
         }
     }
 }
