951a779442d76a28d3a162107eb2423f95a0abe7
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-22 19:11:36 -0400

    Implemented Cover Type Unit Of Work Repositories

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,9 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        
+        ICoverTypeRepository CoverType { get; }
+
 
         void Save();
     }
