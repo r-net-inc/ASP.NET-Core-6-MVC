c57ab0449d1d37b3d40b975306bc0269a1cbf2e1
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-22 14:32:32 -0400

    Revert "Minor Fix"
    This reverts commit 0e9280b2c81243f893c910e813aef4592130e28c.

--- BulkyBook.Models/CoverType.cs
+++ BulkyBook.Models/CoverType.cs
@@ -12,9 +12,7 @@ namespace BulkyBook.Models
         [Key]
         public int Id { get; set; }
 
-        [Display(Name = "Cover Type")]
         [Required]
-        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
