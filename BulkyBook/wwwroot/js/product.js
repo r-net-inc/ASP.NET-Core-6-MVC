7aeac6e4bdb495819072f6071ea52f7df6f9363a
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-24 15:32:09 -0400

    Added Delete Products

--- BulkyBook/wwwroot/js/product.js
+++ BulkyBook/wwwroot/js/product.js
@@ -1,54 +1,85 @@
-﻿$(document).ready(function () {
+﻿var dataTable;
+
+$(document).ready(function () {
     loadDataTable()
 });
 
 function loadDataTable() {
-    let datatable = $('#products').DataTable({
-        'ajax': {
-            'url': '/Admin/Products/GetAll'
+    dataTable = $('#products').DataTable({
+        ajax: {
+            url: '/Admin/Products/GetAll'
         },
-        'columns': [
-            { 'data': 'title', 'width': '15%' },
-            { 'data': 'author', 'width': '15%' },
-            { 'data': 'category.name', 'width': '15%' },
-            { 'data': 'isbn', 'width': '15%' },
-            { 'data': 'price', 'width': '15%' },
+        columns: [
+            { data: 'title', width: '15%' },
+            { data: 'author', width: '15%' },
+            { data: 'category.name', width: '15%' },
+            { data: 'isbn', width: '15%' },
+            { data: 'listPrice', width: '15%' },
             {
-                'data': 'id',
-                'render': function (data) {
+                data: 'id',
+                render: function (data) {
                     return `
                         <div class="btn-group" role="group">
                             <a href="/Admin/Products/Upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
-                            <a class="btn btn-danger mx-2 js-delete" data-product-id="${data}"><i class="bi bi-trash3"></i>Delete</a>
+                            <a onClick=Delete('/Admin/Products/Delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash3"></i>Delete</a>
                         </div>
                     `
                 },
-                'width': '15%'
+                width: '15%'
             }
         ]
     });
 }
 
-$(document).on('click', '.js-delete', function () {
+function Delete(url) {
     var button = $(this);
 
-    bootbox.confirm({
-        title: 'Delete Product?',
-        message: 'Are you sure you want to delete this product?',
-        buttons: {
-            cancel: {
-                label: '<i class="bi bi-x-square"></i>&nbsp; No',
-                className: 'btn-success'
-            },
-            confirm: {
-                label: '<i class="bi bi-check-square"></i>&nbsp; Yes',
-                className: 'btn-danger'
-            }
-        },
-        callback: function (result) {
-            if (result) {
-                window.location.assign("/Admin/Products/Delete/" + button.attr("data-product-id"));
-            }
+    swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover this product!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((result) => {
+        if (result) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        //dataTable.ajax.reload();
+                        dataTable.row(button.parents("tr")).remove().draw();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
         }
-    });
-})
+    })
+}
+
+//$(document).on('click', '.js-delete', function () {
+//    var button = $(this);
+
+//    bootbox.confirm({
+//        title: 'Delete Product?',
+//        message: 'Are you sure you want to delete this product?',
+//        buttons: {
+//            cancel: {
+//                label: '<i class="bi bi-x-square"></i>&nbsp; No',
+//                className: 'btn-success'
+//            },
+//            confirm: {
+//                label: '<i class="bi bi-check-square"></i>&nbsp; Yes',
+//                className: 'btn-danger'
+//            }
+//        },
+//        callback: function (result) {
+//            if (result) {
+//                window.location.assign("/Admin/Products/Delete/" + button.attr("data-product-id"));
+//            }
+//        }
+//    });
+//})
