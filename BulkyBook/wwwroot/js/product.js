f3c2a6566a8eadb169d4bcde1e2c7bc9df7582a9
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-23 22:46:00 -0400

    Loaded DataTables form Products Index

---
+++ BulkyBook/wwwroot/js/product.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    loadDataTable()
+});
+
+function loadDataTable() {
+    let datatable = $('#products').DataTable({
+        'ajax': {
+            'url': '/Admin/Products/GetAll'
+        },
+        'columns': [
+            { 'data': 'title', 'width': '15%' },
+            { 'data': 'author', 'width': '15%' },
+            { 'data': 'isbn', 'width': '15%' },
+            { 'data': 'price', 'width': '15%' },
+        ]
+    });
+}
