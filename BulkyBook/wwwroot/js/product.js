59f219de4751ebf813ed08370f88aec7fa7a2585
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-23 22:57:49 -0400

    Minor Fixes

--- BulkyBook/wwwroot/js/product.js
+++ BulkyBook/wwwroot/js/product.js
@@ -15,3 +15,27 @@ function loadDataTable() {
         ]
     });
 }
+
+$(document).on('click', '.js-delete', function () {
+    var button = $(this);
+
+    bootbox.confirm({
+        title: 'Delete Product?',
+        message: 'Are you sure you want to delete this product?',
+        buttons: {
+            cancel: {
+                label: '<i class="bi bi-x-square"></i>&nbsp; No',
+                className: 'btn-success'
+            },
+            confirm: {
+                label: '<i class="bi bi-check-square"></i>&nbsp; Yes',
+                className: 'btn-danger'
+            }
+        },
+        callback: function (result) {
+            if (result) {
+                window.location.assign("/Admin/Products/Delete/" + button.attr("data-product-id"));
+            }
+        }
+    });
+})
