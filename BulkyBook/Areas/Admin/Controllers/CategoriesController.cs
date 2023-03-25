fb98c7051a7e02dbec503157e0b8371510bf655f
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-25 03:35:31 -0400

    Added Upsert Action to Categories

--- BulkyBook/Areas/Admin/Controllers/CategoriesController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoriesController.cs
@@ -12,10 +12,8 @@ namespace BulkyBook.Areas.Admin.Controllers
     [Area("Admin")]
     public class CategoriesController : Controller
     {
-        //private readonly ApplicationDbContext _db;
         private readonly IUnitOfWork _unitOfWork;
 
-        //public CategoriesController(ApplicationDbContext db)
         public CategoriesController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -23,7 +21,6 @@ namespace BulkyBook.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            //IEnumerable<Category> categories = _db.Categories;
             IEnumerable<Category> categories = _unitOfWork.Category.GetAll();
 
             return View(categories);
@@ -39,111 +36,35 @@ namespace BulkyBook.Areas.Admin.Controllers
             }
             else
             {
-                // Update Product
-                //ViewBag.Title = "Edit Product";
                 var category = _unitOfWork.Category.GetFirstOrDefault(p => p.Id == id);
                 return View(category);
             }
         }
 
-        //// GET
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                    TempData["success"] = "Category created successfully!";
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                    TempData["success"] = "Category updated successfully!";
+                }
 
-        //// POST
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Create(Category category)
-        //{
-        //    if (category.Name == category.DisplayOrder.ToString())
-        //    {
-        //        ModelState.AddModelError("name", "The Name cannot exactly match the Display Order.");
-        //    }
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
 
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(category);
-        //    }
-
-        //    //_db.Categories.Add(category);
-        //    _unitOfWork.Category.Add(category);
-        //    //_db.SaveChanges();
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "Category created successfully";
-
-        //    return RedirectToAction("Index");
-        //}
-
-        //// GET
-        //public IActionResult Edit(int? id)
-        //{
-        //    if (id == null || id == 0)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    //var categoryFromDb = _db.Categories.Find(id);
-        //    //var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
-        //    //var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
-        //    var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(c => c.Id == id);
-
-        //    if (categoryFromDbFirst == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(categoryFromDbFirst);
-        //}
-
-        //// POST
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Update(Category category)
-        //{
-        //    if (category.Name == category.DisplayOrder.ToString())
-        //    {
-        //        ModelState.AddModelError("name", "The Name cannot exactly match the Display Order.");
-        //    }
-
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(category);
-        //    }
-
-        //    //_db.Categories.Update(category);
-        //    _unitOfWork.Category.Update(category);
-        //    //_db.SaveChanges();
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "Category updated successfully";
-
-        //    return RedirectToAction("Index");
-        //}
-
-        //public IActionResult Delete(int? id)
-        //{
-        //    if (id == null || id == 0)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    //var categoryInDb = _db.Categories.Find(id);
-        //    var categoryInDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-
-        //    if (categoryInDb == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    //_db.Categories.Remove(categoryInDb);
-        //    _unitOfWork.Category.Remove(categoryInDb);
-        //    //_db.SaveChanges();
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "Category deleted successfully";
-
-        //    return RedirectToAction("Index");
-        //}
+            return View(category);
+        }
 
         #region API CALLS
         [HttpGet]
@@ -168,10 +89,8 @@ namespace BulkyBook.Areas.Admin.Controllers
                 return Json(new { success = false, message = "Error while deleting!" });
             }
 
-            //_db.Categories.Remove(categoryInDb);
             _unitOfWork.Category.Remove(categoryInDb);
             TempData["success"] = "Category deleted successfully!";
-            //_db.SaveChanges();
             _unitOfWork.Save();
 
             return Json(new { success = true, message = "Category deleted successfully!" });
