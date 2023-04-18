737066d304ee9f10eecb68d71bfa0e3bac46d128
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-04-18 16:57:22 -0400

    Started OrdersController and Added GetAll API

---
+++ BulkyBook/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+	public class OrdersController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrdersController(IUnitOfWork unitOfWork)
+        {
+			_unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+			return Json(new { data = orderHeaders });
+		}
+		#endregion
+	}
+}
